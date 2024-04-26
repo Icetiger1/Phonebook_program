fb1f3a8f3c5e7419fc720404ba577dc35866c259
Anastasia <121202527+Icetiger1@users.noreply.github.com>
2024-04-26 16:21:54 +0300

    Update SQLQuery.sql

--- Phonebook_program/SQLQuery.sql
+++ Phonebook_program/SQLQuery.sql
@@ -3,19 +3,19 @@
 use PhonebookBD;
 CREATE TABLE Contact
 (
-    Id INT identity primary key not null,
+    Id INT IDENTITY PRIMARY KEY NOT NULL,
     Name VARCHAR(255) NOT NULL,
 	Surname VARCHAR(255) NOT NULL,
-    Age INT null,
-	Email VARCHAR(255) not null,
-	PhoneNumber VARCHAR(255) not null,
-	Address VARCHAR(255) null,
-	City VARCHAR(255) null,
-	Region VARCHAR(255) null,
-	PostalCode INT null,
-    Country VARCHAR(255) null
+    Age INT NULL,
+	Email VARCHAR(255) NOT NULL,
+	PhoneNumber VARCHAR(255) NOT NULL,
+	Address VARCHAR(255) NULL,
+	City VARCHAR(255) NULL,
+	Region VARCHAR(255) NULL,
+	PostalCode INT NULL,
+    Country VARCHAR(255) NULL
 );
 
-insert Contact Values('Person1', 'Person1', '35', 'Person1@mail.ru', '8(916)999-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
-insert Contact Values('Person2', 'Person2', '25', 'Person2@mail.ru', '8(916)888-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
-insert Contact Values('Person3', 'Person3', '33', 'Person3@mail.ru', '8(916)777-99-99', '', 'London', 'London', '', 'Great Britain')
+INSERT Contact VALUES('Person1', 'Person1', '35', 'Person1@mail.ru', '8(916)999-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
+INSERT Contact VALUES('Person2', 'Person2', '25', 'Person2@mail.ru', '8(916)888-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
+INSERT Contact VALUES('Person3', 'Person3', '33', 'Person3@mail.ru', '8(916)777-99-99', '', 'London', 'London', '', 'Great Britain')

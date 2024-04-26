b531dd9133bef91fcc06e1362a018f39d95a186c
Anastasia <121202527+Icetiger1@users.noreply.github.com>
2024-04-26 16:22:48 +0300

    Update SQLQuery.sql

--- Phonebook_program/SQLQuery.sql
+++ Phonebook_program/SQLQuery.sql
@@ -16,6 +16,6 @@ CREATE TABLE Contact
     Country VARCHAR(255) NULL
 );
 
-INSERT Contact VALUES('Person1', 'Person1', '35', 'Person1@mail.ru', '8(916)999-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
-INSERT Contact VALUES('Person2', 'Person2', '25', 'Person2@mail.ru', '8(916)888-99-99', '', 'Moscow', 'Moscow', '', 'Russia')
-INSERT Contact VALUES('Person3', 'Person3', '33', 'Person3@mail.ru', '8(916)777-99-99', '', 'London', 'London', '', 'Great Britain')
+INSERT Contact VALUES('Person1', 'Person1', '35', 'Person1@mail.ru', '8(916)999-99-99', '', 'Moscow', 'Moscow', '', 'Russia');
+INSERT Contact VALUES('Person2', 'Person2', '25', 'Person2@mail.ru', '8(916)888-99-99', '', 'Moscow', 'Moscow', '', 'Russia');
+INSERT Contact VALUES('Person3', 'Person3', '33', 'Person3@mail.ru', '8(916)777-99-99', '', 'London', 'London', '', 'Great Britain');
